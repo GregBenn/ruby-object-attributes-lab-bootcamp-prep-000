63faf4b992eebe96247ba4f7bf38251c968894ba
Greg Benn <gbenn@powerhrg.com>
2018-05-06 21:27:42 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,8 +8,21 @@ class Dog
     @name
   end
 
-  def name=(new_name)
-    @name = new_name
+  # def name=(new_name)
+  #   @name = new_name
+  # end
+
+  def initialize(breed)
+    @breed = name
   end
+
+  def breed
+    @breed
+  end
+
+  # def breed=(new_breed)
+  #   @breed = new_breed
+  # end
+
 
 end
